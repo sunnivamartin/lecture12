6aa31ed4dd5b6ec2915b75df5c313f8a8b4d80a1
sunnivamartin <suma013@student.kristiania.no>
2026-04-13 14:50:11 +0200

    fixed some stuff

---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono();
+// `serveStatic` makes Hono serve the output from `vite build`
+app.get("*", serveStatic({ root: "../dist" }));
+serve(app);
